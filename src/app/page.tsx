017d7dd3d79a7ddc4350f18762871131d4cf50ba
Shubham <aaronjonchhe@gmail.com>
2024-11-03 12:40:04 +0545

    Customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/components/ui/button"
-import { TestComponent } from "@/features/test"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <div>
-    <TestComponent></TestComponent>
+    <div className="flex gap-4">
+      <Input></Input>
+      <Button size="xs">
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
     </div>
   )
 }
